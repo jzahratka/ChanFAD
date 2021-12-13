bcb552200c67d8dcc61024e4b4938163597db299
jzahratka <jzahratk@bw.edu>
2021-12-13 15:29:38 -0500

    Updated settings file with allowed hosts.

--- ChanFAD/settings.py
+++ ChanFAD/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chanfad.herokuapp.com','127.0.0.1']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
